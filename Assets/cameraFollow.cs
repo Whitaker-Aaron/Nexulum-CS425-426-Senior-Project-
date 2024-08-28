d5e98db5666f3820193990d4d6b82e54fa25884b
Spencer garcia <garciaspencer06@gmail.com>
2024-08-28 11:17:01 -0700

    Camera update
    added new camera follow player script

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = .125f;
+    public Vector3 offset;
+
+    bool found = false;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (!found)
+        {
+            if (GameObject.FindWithTag("Player") == null)
+                return;
+
+            target = GameObject.FindWithTag("Player").transform;
+            found = true;
+        }
+        else
+            return;
+    }
+
+    void LateUpdate()
+    {
+
+        if (target == null)
+            return;
+        else
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothPosition;
+            transform.LookAt(target);
+        }
+
+    }
+
+
+}
+
+
