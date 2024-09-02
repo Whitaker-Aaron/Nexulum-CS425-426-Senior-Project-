64f5ab81d3308ab8aa9d66425f8c2a8693d82260
Aaron Whitaker <aaronwhitaker690@gmail.com>
2024-09-01 19:55:59 -0700

    Added list to Scroll Manager

--- Assets/Scripts/Craft Materials/Material Scroll Manager.cs
+++ Assets/Scripts/Craft Materials/Material Scroll Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -9,6 +10,9 @@ public class MaterialScrollManager : MonoBehaviour
     [SerializeField] ScrollView scrollView;
     [SerializeField] GameObject content;
     [SerializeField] GameObject testContent;
+    [SerializeField] Texture testTexture;
+
+    List<GameObject> currentMaterials = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
@@ -25,14 +29,34 @@ public class MaterialScrollManager : MonoBehaviour
     {
         //Test code
         var scrollObject = testContent.GetComponent<MaterialScrollObject>();
-        scrollObject.description.text = "Sapphire Bar";
+        //scrollObject.description.text = "Sapphire Bar";
         //scrollObject.description.color = new Color(56, 56, 56, 20.0f);
-        scrollObject.quantity.text = "x" + "3";
+        //scrollObject.quantity.text = "x" + "3";
+        //scrollObject.imageRef.texture = testTexture;
         //scrollObject.quantity.color = new Color(56, 56, 56, 20.0f);
-        Instantiate(testContent).transform.SetParent(content.transform);
-        
+        //Instantiate(testContent).transform.SetParent(content.transform);
+
+        scrollObject = testContent.GetComponent<MaterialScrollObject>();
+        scrollObject.description.text = "Sapphire";
+        scrollObject.imageRef.texture = testTexture;
+        //scrollObject.description.color = new Color(56, 56, 56, 20.0f);
+        scrollObject.quantity.text = "x" + "2";
+        //scrollObject.quantity.color = new Color(56, 56, 56, 20.0f);
+        GameObject newScrollMaterial = Instantiate(testContent);
+        newScrollMaterial.transform.SetParent(content.transform);
+        //.transform.SetParent(content.transform);
+
+        currentMaterials.Add(newScrollMaterial);
+        currentMaterials[0].GetComponent<MaterialScrollObject>().quantity.text = "x" + "4";
 
         //color = 0.5f;
         //testContent.transform.parent = content.transform;
+    }
+
+    private void OnDestroy()
+    {
+        var scrollObject = testContent.GetComponent<MaterialScrollObject>();
+        scrollObject.description.text = "empty";
+        scrollObject.quantity.text = "x0";
     }
 }
