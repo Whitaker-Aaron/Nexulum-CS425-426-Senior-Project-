a1903bcc3b6027096f19db7bc887b4063274dca7
Spencer garcia <garciaspencer06@gmail.com>
2024-09-05 11:59:59 -0700

    added dmg over time and instantiation of sword

--- Assets/swordCombat.cs
+++ Assets/swordCombat.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class swordCombat : MonoBehaviour
 {
-    public int damage = 100;
+    public int damage = 50;
+
+    //rune ability combat mechanic
+    public bool isFire = false;
+    public int fireDmg = 10;
+    public float fireTime = 5f;
+    public float fireDmgInterval = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFire = true;
     }
 
     // Update is called once per frame
@@ -18,11 +24,19 @@ public class swordCombat : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Enemy")
         {
+            if(isFire)
+            {
+                other.GetComponent<EnemyFrame>().StartCoroutine(other.GetComponent<EnemyFrame>().dmgOverTime(fireDmg, fireTime, fireDmgInterval));
+            }
             other.GetComponent<EnemyFrame>().takeDamage(damage);
+            return;
         }
+
     }
+
+    
 }
