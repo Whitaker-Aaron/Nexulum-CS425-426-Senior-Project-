86da12fcb175377ba1aa53b0a28962aa269653c7
Spencer garcia <garciaspencer06@gmail.com>
2024-09-06 20:40:00 -0700

    small marge update

--- Assets/Scripts/animation/enemyAnimInterface.cs
+++ Assets/Scripts/animation/enemyAnimInterface.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public interface EnemyAnimation
+{
+    void setMovement(float forward, float turn);
+
+}
+
 public class enemyAnimInterface : MonoBehaviour
 {
+
+    private Animator anim;
+    private readonly int forwardHash = Animator.StringToHash("Forward");
+    private readonly int turnHash = Animator.StringToHash("Turn");
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+    }
+
+    public void SetMovement(float forward, float turn)
+    {
+        anim.SetFloat(forwardHash, forward);
+        anim.SetFloat(turnHash, turn);
     }
 
     // Update is called once per frame
