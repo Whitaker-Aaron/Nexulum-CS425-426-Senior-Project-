67c006898e3700886530c9c30684b977682d04ea
Aaron Whitaker <aaronwhitaker690@gmail.com>
2024-09-12 15:07:50 -0700

    Added CraftMenu prefabs

--- Assets/CraftMenuTransition.cs
+++ Assets/CraftMenuTransition.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CraftMenuTransition : MonoBehaviour
 {
     // Start is called before the first frame update
+    GameObject mainButtons;
+    GameObject mainSelection; 
     void Start()
     {
-        
+        mainButtons = GameObject.Find("MainButtons");
+        mainSelection = GameObject.Find("MainSelection");
     }
 
     // Update is called once per frame
@@ -18,8 +21,29 @@ public class CraftMenuTransition : MonoBehaviour
 
     public void NavigateToMaterialMenu()
     {
-        Debug.Log("Button pressed");
+        Debug.Log("Back Button pressed");
         GameObject.Find("MenuManager").GetComponent<MenuManager>().navigateToMaterialMenu();
+    }
+
+    public void NavigateToWeaponCraftMenu()
+    {
+        Debug.Log("Weapon Button pressed");
+        mainButtons.SetActive(false);
+        mainSelection.SetActive(false);
+    }
+
+    public void NavigateToRunesCraftMenu()
+    {
+        Debug.Log("Rune Button pressed");
+        mainButtons.SetActive(false);
+        mainSelection.SetActive(false);
+    }
+
+    public void NavigateToItemsCraftMenu()
+    {
+        Debug.Log("Items Button pressed");
+        mainButtons.SetActive(false);
+        mainSelection.SetActive(false);
     }
 
 
