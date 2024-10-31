d2a46e88e3d06d847e0b46837b6d3dc1909b4e71
Spencer garcia <garciaspencer06@gmail.com>
2024-10-30 22:17:50 -0700

    enemy mage addition

---
+++ Assets/enemyMage.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMage : MonoBehaviour, mageInterface
+{
+
+    public float detectionRadius;
+
+    private List<GameObject> enemies;
+
+    private GameObject player;
+
+    public LayerMask playerLayer;
+
+    private bool canCastSpell = true;
+
+    public float castTime = 3f;
+
+    [SerializeField]
+    private Transform spellProjSpawn;
+
+    private void Awake()
+    {
+        //gameObject.GetComponent<SphereCollider>().radius = detectionRadius;
+        enemies = new List<GameObject>();
+        player = null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        detectPlayer();
+
+        if(player != null)
+        {
+            gameObject.transform.LookAt(player.transform.position, Vector3.up);
+        }
+    }
+
+
+
+    public IEnumerator spellCast()
+    {
+        canCastSpell = false;
+        projectileManager.Instance.getProjectile("enemyMagePoolOne", spellProjSpawn.position, spellProjSpawn.rotation);
+        yield return new WaitForSeconds(castTime);
+        canCastSpell = true;
+        yield break;
+    }
+
+    public void detectPlayer()
+    {
+        Collider[] cols = Physics.OverlapSphere(gameObject.transform.position + new Vector3(0,1,0), detectionRadius);
+
+        foreach(Collider col in cols)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                player = col.gameObject;
+            }
+            else if (col.gameObject.tag == "Enemy" && col.gameObject != gameObject)
+            {
+                enemies.Add(col.gameObject);
+            }
+        }
+
+        if(player != null)
+        {
+            Vector3 temp = (player.transform.position - gameObject.transform.position).normalized;
+            Ray ray = new Ray(transform.position + new Vector3(0, 0.75f, 0), temp);
+            RaycastHit[] hits = Physics.RaycastAll(ray, detectionRadius);
+
+            foreach (RaycastHit hit in hits)
+            {
+                // Check if the hit object is the player
+                if (hit.collider.CompareTag("Player") || hit.collider.CompareTag("Enemy"))
+                {
+                    if (canCastSpell)
+                    {
+                        StartCoroutine(spellCast());
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        // Visualize detection range in the editor
+        Gizmos.color = Color.red;
+        if (player != null)
+        {
+            Vector3 temp = (player.transform.position - gameObject.transform.position).normalized;
+            Gizmos.DrawRay(transform.position + new Vector3(0, 0.75f, 0), temp * detectionRadius);
+        }
+    }
+
+}
