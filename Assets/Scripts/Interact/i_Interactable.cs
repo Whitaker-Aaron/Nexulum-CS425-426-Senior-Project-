ef69dbd551310fb5bad1e7f5aab449e8fe58a226
MrBoatCraft <stephenwest@unr.edu>
2024-11-03 17:04:03 -0800

    Script Errors

--- Assets/Scripts/Interact/i_Interactable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public interface i_Interactable
-{
-    string interactionPrompt { get; } 
-    bool Interact(Interactor interactor);
-    void ShowUI();
-    void HideUi();
-}
