eae1c28e47f3bcecd973e3b1600e62aefc5e3405
MrBoatCraft <stephenwest@unr.edu>
2024-11-08 20:52:54 -0800

    Added Spinning AxeBlade Traps

---
+++ Assets/Scripts/Traps/SpinningBlades.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningBlades : MonoBehaviour , i_Trap
+{
+    private Animator animator;
+    private Vector3 initialPosition;
+    private Vector3 previousPosition;
+    public int damageAmount;
+    public enum SpinDirection { Left, Right }
+    public SpinDirection Direction;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        initialPosition = transform.position;
+        previousPosition = transform.position;
+    }
+
+    void Update()
+    {
+        DetectMovementDirection();
+        SpinningAnimation();
+    }
+
+    private void DetectMovementDirection()
+    {
+        if (transform.position.x < previousPosition.x)
+        {
+            Direction = SpinDirection.Left;
+        }
+        else if (transform.position.x > previousPosition.x)
+        {
+            Direction = SpinDirection.Right;
+        }
+        previousPosition = transform.position;
+    }
+
+    private void SpinningAnimation()
+    {
+        if (Direction == SpinDirection.Left)
+        {
+            animator.SetBool("isLeft", true);
+        }
+        else if (Direction == SpinDirection.Right)
+        {
+            animator.SetBool("isLeft", false);
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        CharacterBase playerHealth = other.GetComponent<CharacterBase>();
+
+        if (playerHealth != null)
+        {
+            playerHealth.takeDamage(damageAmount, Vector3.zero);
+            Debug.Log("Player hit by spikes and took " + damageAmount + " damage.");
+        }
+    }
+
+}
