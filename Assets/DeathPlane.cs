9e89c8d7486cdd45a810d54fc7929b7bc0b6ec2d
Aaron Whitaker <aaronwhitaker690@gmail.com>
2025-01-02 17:08:49 -0800

    Dungeon Refactoring (Rooms 8, 9, 10, 11)

--- Assets/DeathPlane.cs
+++ Assets/DeathPlane.cs
@@ -27,24 +27,30 @@ public class DeathPlane : MonoBehaviour
             var character = other.gameObject.GetComponent<CharacterBase>();
             //camera.PauseFollow();
             //camera.PauseLookAt();
-            camera.positionTarget = character.lastGroundLocation;    
+            camera.positionTarget = character.lastGroundLocation;
+            character.GetMasterInput().GetComponent<masterInput>().pausePlayerInput();
+            GameObject.Find("MenuManager").GetComponent<MenuManager>().menusPaused = true;
+            StartCoroutine(GameObject.Find("LifetimeManager").GetComponent<LifetimeManager>().AnimateRoomTransition());
             StartCoroutine(WaitThenTakeDamage(character));
         }
     }
 
     public IEnumerator WaitThenTakeDamage(CharacterBase character)
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.3f);
         camera.SetCameraMode(CameraFollow.FollowMode.PositionLerp);
         yield return new WaitForSeconds(0.1f);
         camera.SetCameraMode(CameraFollow.FollowMode.Lerp);
         character.ResetToGround();
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(0.75f);
+
         if (!character.transitionedRoom && !character.transitioningRoom)
         {
             character.takeDamage(20, Vector3.zero);
             //camera.UnpauseFollow();
             //camera.UnpauseLookAt();
         }
+        character.GetMasterInput().GetComponent<masterInput>().resumePlayerInput();
+        GameObject.Find("MenuManager").GetComponent<MenuManager>().menusPaused = false;
     }
 }
