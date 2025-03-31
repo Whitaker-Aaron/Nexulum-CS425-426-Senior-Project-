4f8d6523418f244d96998b042ba151a3a5dff4ec
MrBoatCraft <89425205+1204-West-Stephen@users.noreply.github.com>
2025-03-30 20:28:21 -0700

    quick fix

--- Assets/Scripts/Traps/SwingingTrap.cs
+++ Assets/Scripts/Traps/SwingingTrap.cs
@@ -1,24 +1,38 @@
 using UnityEngine;
 
+public enum SwingDirection { X, Y, Z};
+
 public class SwingingAxeTrap : MonoBehaviour , i_Trap
 {
     [Header("Swing Settings")]
     public float swingSpeed = 1f;
     public float swingAngle = 45f;
     public int damageAmount = 20;
+    public SwingDirection swingDirection;
 
-    private float initialRotationZ;
+    private float initialRotation;
 
     void Start()
     {
-        initialRotationZ = transform.localEulerAngles.z;
+        if (swingDirection == SwingDirection.X)
+        {
+            initialRotation = transform.localEulerAngles.x;
+        }
+        else if (swingDirection == SwingDirection.Y)
+        {
+            initialRotation = transform.localEulerAngles.z;
+        }
+        else if (swingDirection == SwingDirection.Z)
+        {
+            initialRotation = transform.localEulerAngles.z;
+        }
     }
 
     void Update()
     {
         float angle = Mathf.Sin(Time.time * swingSpeed) * swingAngle;
 
-        transform.localRotation = Quaternion.Euler(0, 0, initialRotationZ + angle);
+        transform.localRotation = Quaternion.Euler(0, 0, initialRotation + angle);
     }
 
     public void OnTriggerEnter(Collider other)
